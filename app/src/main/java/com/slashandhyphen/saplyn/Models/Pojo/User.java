c35fb73a81fee4e1ab87a85d936a9c59a39327ab
Mike <mthomsonbrown@gmail.com>
2016-09-30 20:21:21 -0700

    Conservative polish

--- app/src/main/java/com/slashandhyphen/saplyn/Models/Pojo/User.java
+++ app/src/main/java/com/slashandhyphen/saplyn/Models/Pojo/User.java
@@ -2,6 +2,12 @@ package com.slashandhyphen.saplyn.Models.Pojo;
 
 import android.util.Log;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by Mike on 9/17/2016.
  */
@@ -12,10 +18,10 @@ public class User {
 
     public Integer id;
     public String email;
-    public String createdAt;
+    private String createdAt;
     public String updatedAt;
-    public String authToken;
-    public String username;
+    private String authToken;
+    private String username;
 
     public User(String authToken) {
         this.authToken = authToken;
@@ -28,9 +34,28 @@ public class User {
         return this.username;
     }
 
-    public String getLiveTime() {
-        // Needs to be fixed to actually return time this thing has been used, but it's a POC so I
-        // don't really care.
-        return createdAt;
+    public String getCreatedAt() {
+        // TODO Definition should be refactored up
+        String datePresentationFormat = "EEE MMM dd, yyyy GG";
+
+        return new SimpleDateFormat(datePresentationFormat, new Locale("en_US")).
+                format(railsTimestampToDate(createdAt));
+    }
+
+    // TODO Class should be refactored up
+    private Date railsTimestampToDate(String railsDate) {
+        String railsFormat = "yyyy-MM-dd'T'HH:mm:ss";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(railsFormat, new Locale("en_US"));
+        Date date = null;
+        try {
+            // TODO Enter terrible hacks.  I don't care about milliseconds, and that's where the
+            // parse function is throwing a parse exception, so I will remove that bit:
+            String hackyDate = railsDate.substring(0, 18);
+
+            date = dateFormat.parse(hackyDate);
+        } catch (ParseException e) {
+            Log.d(TAG, "railsTimestampToDate: " + e.getMessage());
+        }
+        return date;
     }
 }
--- app/src/main/java/com/slashandhyphen/saplyn/VladimirActivity.java
+++ app/src/main/java/com/slashandhyphen/saplyn/VladimirActivity.java
@@ -3,8 +3,6 @@ package com.slashandhyphen.saplyn;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.slashandhyphen.saplyn.Models.Pojo.User;
@@ -14,16 +12,11 @@ import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
-import static com.slashandhyphen.saplyn.R.id.mr_button;
-
-public class VladimirActivity extends AppCompatActivity implements View.OnClickListener {
+public class VladimirActivity extends AppCompatActivity {
     private static String TAG = "~Vladimir~";
-    Button mrButton;
-    Button mrCreateTime;
     TextView fakeText;
     TextView fakeCreated;
     private Observable<User> userListener;
-    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,45 +25,21 @@ public class VladimirActivity extends AppCompatActivity implements View.OnClickL
         SaplynService saplynService = new SaplynService();
 
         fakeText = (TextView) findViewById(R.id.hello);
-        mrButton = (Button) findViewById(mr_button);
-        mrButton.setOnClickListener(this);
-
         fakeCreated = (TextView) findViewById(R.id.created);
-        mrCreateTime = (Button) findViewById(R.id.mr_create_time);
-        mrCreateTime.setOnClickListener(this);
 
         userListener = saplynService.viewUser();
+        populateUser();
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.mr_button:
-                doMrButton();
-                break;
-            case R.id.mr_create_time:
-                doCreatedTime();
-                break;
-        }
-    }
-
-    private void doCreatedTime() {
+    private void populateUser() {
         userListener.subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(user -> {
                             fakeText.setText(user.getUsername());
+                            fakeCreated.setText(user.getCreatedAt());
                         },
                         throwable -> Log.e(TAG, "onErrorFromTheGetLiveTime: "
                                 + throwable.getMessage()));
     }
 
-    private void doMrButton() {
-        userListener.subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(user -> {
-                            fakeCreated.setText(user.getLiveTime());
-                        },
-                        throwable -> Log.e(TAG, "onErrorFromTheGETUserCall: "
-                                + throwable.getMessage()));
-    }
 }
